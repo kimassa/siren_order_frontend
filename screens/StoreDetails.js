ac6194d6d614bb9beb4dd32eaaf89dc403dfc700
seongto <smalllife@naver.com>
2019-06-26 03:19:55 +0900

    점포 상세정보 스크린 구현.
    점포 리스트에서 클릭 시 뜨는 상세 정보 스크린 구현.
    
    - 정상적으로 리스트에서 아이디를 넘겨 받을 경우 정상 데이터 출력.
    - 받은 아이디가 없을 경우 더미 데이터 출력, 이 경우 주문하기 클릭시 Home
    으로 이동
    - 취소 클릭 시 navigation.goBack() 작동.

--- screens/StoreDetails.js
+++ screens/StoreDetails.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
-  TouchableHighlight,
   View,
-  FlatList
 } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { Button } from 'react-native-elements';
 
 
 let dummyStoreData = {
@@ -31,24 +26,27 @@ export default class StoreDetails extends React.Component {
     this.state ={
       id: '',
       name: '',
+      img_src: '',
       address: '',
-      phon: '',
+      phone: '',
     }
   }
   
 
   componentDidMount(){
+    const { navigation } = this.props;
     this.setState({
       id: navigation.getParam('storeId', 'no_id'),
     },() => {
       console.log("예리님이 보내준 아이디는 : ", this.state);
-      fetch(`http://54.180.153.12:8000/supplier/${store_id}`)
+      fetch(`http://54.180.153.12:8000/supplier/${this.state.id}`)
         .then(response => response.json())
         .catch(()=> dummyStoreData)
         .then(res =>{
           this.setState({
             name : res['name'],
             address : res['address'],
+            img_src : res['img_src'],
             phone : res['phone'],
           }, () => {
             console.log("제대로 데이트 들어가나 : ", this.state);
@@ -61,21 +59,24 @@ export default class StoreDetails extends React.Component {
   
   render(){
     return(
-      <View style={wrapper}>
-        <View style={contentsBox}>
-          <Text style={title}></Text>
+      <View style={styles.wrapper}>
+        <View style={styles.contentsBox}>
+          <Text style={styles.title}>{this.state.name}으로 선택하시겠어요?</Text>
           <Image
-            style={storeImg}
+            style={styles.storeImg}
+            source={{uri:`${this.state.img_src}`}}
           />
 
-          <Text style={storeName}>{this.state.name}</Text>
-          <Text style={storeAddress}>{this.state.address}</Text>
-          <Text style={storeContact}>문의 : {this.state.phone}</Text>
-          <Text style={distanceInfo}>*결제 및 주문은 매장 2km 이내에서 가능합니다.</Text>
+          <Text style={styles.storeName}>{this.state.name}</Text>
+          <Text style={styles.storeAddress}>{this.state.address}</Text>
+          <View style={styles.storeContactWrapper}>
+            <Text style={styles.storeContact}>문의 : {this.state.phone}</Text>
+          </View>
+          <Text style={styles.distanceInfo}>*결제 및 주문은 매장 2km 이내에서 가능합니다.</Text>
 
-          <View style={buttons}>
-            <Button/>
-            <Button/>
+          <View style={styles.buttons}>
+            <Button type="solid" containerStyle={{flex:1, marginRight:6,}} buttonStyle={styles.btnLeft} title="취소" onPress={ () => this.props.navigation.goBack()} />
+            <Button type="solid" containerStyle={{flex:1, marginLeft:6,}} buttonStyle={styles.btnRight} title="주문하기" onPress={ () => this.props.navigation.navigate("Home")} />
           </View>
         </View>
       </View>
@@ -85,22 +86,79 @@ export default class StoreDetails extends React.Component {
 
 const styles = StyleSheet.create({
   wrapper: {
-    width: 100%,
-    height: 100%,
-    backgroundColor: white,
-    justifyContent: center,
-    alignItems: center,
-    padding: 5,
+    flex:1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
   contentsBox:{
-    width: 76%,
-    shadowColor: black,
+    shadowColor: 'black',
     shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.5,
-    shadowRadius: 6,
+    shadowOpacity: 0.3,
+    shadowRadius: 6, borderRadius: 12,
+    backgroundColor: 'white',
+    padding: 20,
+    
+  },
+  title:{
+    fontSize: 16,
+    fontWeight: '700',
+    paddingTop: 24,
+  },
+  storeImg:{
+    alignSelf: 'stretch',
     borderRadius: 4,
-    backgroundColor: white,
-
-
-
+    height: 140,
+    marginTop: 15,
+  },
+  storeName:{
+    marginTop: 20,
+    fontSize: 24,
+    color: '#c18d3a',
+    fontWeight: '900',
+  },
+  storeAddress:{
+    fontSize: 12,
+    borderBottomWidth: 2,
+    borderBottomColor: '#dddddd',
+    paddingVertical: 16,
+    color: '#303030',
+  },
+  storeContactWrapper:{
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderColor: '#dddddd',
+  },
+  storeContact:{
+    fontSize: 14,
+    fontWeight: '700',
+    paddingVertical: 16,
+    color: '#c18d3a',
+  },
+  distanceInfo:{
+    fontSize: 11,
+    color: '#666666',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  buttons:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginVertical: 10,
+  },
+  btnLeft:{
+    fontSize: 16,
+    fontWeight: '500',
+    borderRadius: 4,
+    backgroundColor: '#dddddd',
+    color: '#777777',
+  },
+  btnRight:{
+    fontSize: 16,
+    fontWeight: '500',
+    borderRadius: 4,
+    backgroundColor: '#c18d3a',
+    color: 'white',
+  }
 })
